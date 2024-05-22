8ba69121dfdca4590ffd09a1eef81ef2231c67a2
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-05-22 15:52:59 +0530

    feat: add asset model in apy resource

--- scout/src/core/resources/apy/apy.module.ts
+++ scout/src/core/resources/apy/apy.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ApyService } from './apy.service';
 import { ApyController } from './apy.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Asset, AssetSchema } from '../asset/asset.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Asset.name, schema: AssetSchema }]),
+  ],
   controllers: [ApyController],
   providers: [ApyService],
 })
--- scout/src/core/resources/apy/apy.service.ts
+++ scout/src/core/resources/apy/apy.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Asset } from '../asset/asset.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class ApyService {}
+export class ApyService {
+  constructor(@InjectModel(Asset.name) private assetModel: Model<Asset>) {}
+}
