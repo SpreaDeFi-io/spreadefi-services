0be70ea3d9395dfbe036e32e244c66dcf7f52473
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-07-10 14:33:55 +0530

    feat: add looping leverage

---
+++ scout/src/common/constants/config/looping.ts
@@ -0,0 +1,46 @@
+export const loopingConfig = {
+  Aave: {
+    '42161': {
+      '0x5979D7b546E38E414F7E9822514be443A4800529': {
+        //wstETH
+        leverage: 12,
+      },
+    },
+    '10': {
+      '0x9Bcef72be871e61ED4fBbc7630889beE758eb81D': {
+        //rETH
+        leverage: 10,
+      },
+    },
+    '8453': {
+      '0xc1CBa3fCea344f92D9239c08C0568f6F2F0ee452': {
+        //wstETH
+        leverage: 10,
+      },
+      '0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22': {
+        //cbETH
+        leverage: 7,
+      },
+    },
+  },
+  Seamless: {
+    '8453': {
+      '0xc1CBa3fCea344f92D9239c08C0568f6F2F0ee452': {
+        //wstETH
+        leverage: 10,
+      },
+      '0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22': {
+        //cbETH
+        leverage: 5,
+      },
+    },
+  },
+  Zerolend: {
+    '59144': {
+      '0xB5beDd42000b71FddE22D3eE8a79Bd49A568fC8F': {
+        //wstETH
+        leverage: 2.5,
+      },
+    },
+  },
+};
