79f5962d1f9932afaeedc83d9b2b5773156fe5ae
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-07-10 11:54:50 +0530

    fix: add covalent module

--- scout/src/core/resources/user/balance/balance.module.ts
+++ scout/src/core/resources/user/balance/balance.module.ts
@@ -5,15 +5,16 @@ import { AssetModule } from '../../asset/asset.module';
 import { BalanceLogger } from './balance.logger';
 import { Asset, AssetSchema } from '../../asset/asset.schema';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CovalentService } from 'src/libs/covalent/covalent.service';
+import { CovalentModule } from 'src/libs/covalent/covalent.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Asset.name, schema: AssetSchema }]),
     AssetModule,
+    CovalentModule.register(),
   ],
   controllers: [BalanceController],
-  providers: [BalanceService, BalanceLogger, CovalentService],
+  providers: [BalanceService, BalanceLogger],
   exports: [BalanceService],
 })
 export class BalanceModule {}
