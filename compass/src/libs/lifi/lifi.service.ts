71ed9658161d8efdd3661f7004d9acd139829ff3
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-09-19 19:39:15 +0530

    feat: add lifi module

---
+++ compass/src/libs/lifi/lifi.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@nestjs/common';
+import {
+  ChainId,
+  ContractCallsQuoteRequest,
+  ContractCallsQuoteRequestFromAmount,
+  createConfig,
+  getContractCallsQuote,
+  getQuote,
+  getRoutes,
+  QuoteRequest,
+  RoutesRequest,
+} from '@lifi/sdk';
+import { LIFI_URL } from 'src/common/constants';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class LifiService {
+  constructor(private readonly configService: ConfigService) {
+    createConfig({
+      integrator: 'Spreadefi',
+      apiUrl: LIFI_URL,
+      apiKey: this.configService.get<string>('LIFI_API_KEY'),
+      rpcUrls: {
+        [ChainId.ARB]: [this.configService.get<string>('ARB_RPC')],
+        [ChainId.OPT]: [this.configService.get<string>('OP_RPC')],
+        [ChainId.BAS]: [this.configService.get<string>('BASE_RPC')],
+        [ChainId.FTM]: [this.configService.get<string>('FANTOM_RPC')],
+        [ChainId.AVA]: [this.configService.get<string>('AVALANCHE_RPC')],
+        [ChainId.POL]: [this.configService.get<string>('POLYGON_RPC')],
+        [ChainId.LNA]: [this.configService.get<string>('LINEA_RPC')],
+        [ChainId.BSC]: [this.configService.get<string>('BSC_RPC')],
+      },
+    });
+  }
+
+  async getLifiRoute(lifiRouteArgs: Partial<RoutesRequest>) {
+    const result = await getRoutes({
+      fromAddress: lifiRouteArgs.fromAddress,
+      fromChainId: lifiRouteArgs.fromChainId,
+      toChainId: lifiRouteArgs.toChainId,
+      fromTokenAddress: lifiRouteArgs.fromTokenAddress,
+      toTokenAddress: lifiRouteArgs.toTokenAddress,
+      fromAmount: lifiRouteArgs.fromAmount,
+    });
+
+    return result.routes[0];
+  }
+
+  async getLifiQuote(lifiQuoteArgs: Partial<QuoteRequest>) {
+    const result = await getQuote({
+      fromAddress: lifiQuoteArgs.fromAddress,
+      fromChain: lifiQuoteArgs.fromChain,
+      toChain: lifiQuoteArgs.toChain,
+      fromToken: lifiQuoteArgs.fromToken,
+      toToken: lifiQuoteArgs.toToken,
+      fromAmount: lifiQuoteArgs.fromAmount,
+    });
+
+    return result;
+  }
+
+  async getContractQuote(
+    lifiContractQuoteArgs: Partial<ContractCallsQuoteRequest>,
+  ) {
+    const result = await getContractCallsQuote({
+      fromAddress: lifiContractQuoteArgs.fromAddress,
+      fromChain: lifiContractQuoteArgs.fromChain,
+      toChain: lifiContractQuoteArgs.toChain,
+      fromToken: lifiContractQuoteArgs.fromToken,
+      toToken: lifiContractQuoteArgs.toToken,
+      fromAmount: (lifiContractQuoteArgs as ContractCallsQuoteRequestFromAmount)
+        .fromAmount,
+      contractCalls: lifiContractQuoteArgs.contractCalls,
+    });
+
+    return result;
+  }
+}
