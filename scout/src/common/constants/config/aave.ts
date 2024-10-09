114dfc4ab27f75915f15a2213361171655e5df0d
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-10-09 14:58:31 +0530

    feat: update aave pool data provider address

--- scout/src/common/constants/config/aave.ts
+++ scout/src/common/constants/config/aave.ts
@@ -1,22 +1,22 @@
 export const aaveConfig = {
   '42161': {
     poolAddress: '0x794a61358D6845594F94dc1DB02A252b5b4814aD',
-    poolDataProvider: '0x69FA688f1Dc47d4B5d8029D5a35FB7a548310654',
+    poolDataProvider: '0x7F23D86Ee20D869112572136221e173428DD740B',
   },
   '8453': {
     poolAddress: '0xA238Dd80C259a72e81d7e4664a9801593F98d1c5',
-    poolDataProvider: '0x2d8A3C5677189723C4cB8873CfC9C8976FDF38Ac',
+    poolDataProvider: '0xd82a47fdebB5bf5329b09441C3DaB4b5df2153Ad',
   },
   '10': {
     poolAddress: '0x794a61358D6845594F94dc1DB02A252b5b4814aD',
-    poolDataProvider: '0x69FA688f1Dc47d4B5d8029D5a35FB7a548310654',
+    poolDataProvider: '0x7F23D86Ee20D869112572136221e173428DD740B',
   },
   '534352': {
     poolAddress: '0x11fCfe756c05AD438e312a7fd934381537D3cFfe',
-    poolDataProvider: '0xa99F4E69acF23C6838DE90dD1B5c02EA928A53ee',
+    poolDataProvider: '0xe2108b60623C6Dcf7bBd535bD15a451fd0811f7b',
   },
   '1088': {
     poolAddress: '0x90df02551bB792286e8D4f13E0e357b4Bf1D6a57',
-    poolDataProvider: '0x99411FC17Ad1B56f49719E3850B2CDcc0f9bBFd8',
+    poolDataProvider: '0xC01372469A17b6716A38F00c277533917B6859c0',
   },
 };
